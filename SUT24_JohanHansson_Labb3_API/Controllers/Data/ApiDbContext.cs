c7bc16510f6116ef3927200b983ceddcca58d11c
johan <johan@LAPTOP-6KO4S5LR>
2025-04-28 13:23:50 +0200

    Building Controllers

---
+++ SUT24_JohanHansson_Labb3_API/Controllers/Data/ApiDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SUT24_JohanHansson_Labb3_API.Models;
+
+namespace SUT24_JohanHansson_Labb3_API.Controllers.Data
+{
+    public class ApiDbContext:DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<PersonInterest> PersonInterests { get; set; }
+        public DbSet<Link> Links { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonInterest>()
+                .HasKey(pi => new { pi.PersonId, pi.InterestId });
+
+            modelBuilder.Entity<PersonInterest>()
+                .HasOne(pi => pi.Person)
+                .WithMany(p => p.PersonInterests)
+                .HasForeignKey(pi => pi.PersonId);
+
+            modelBuilder.Entity<PersonInterest>()
+                .HasOne(pi => pi.Interest)
+                .WithMany(i => i.PersonInterests)
+                .HasForeignKey(pi => pi.InterestId);
+        }
+    }
+}
+
---
+++ SUT24_JohanHansson_Labb3_API/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SUT24_JohanHansson_Labb3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+    }
+}
