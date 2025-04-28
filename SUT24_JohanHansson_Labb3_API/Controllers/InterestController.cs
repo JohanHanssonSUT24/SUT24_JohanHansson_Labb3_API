b9bffe92387529117440175e99547ac51fa873c7
johan <johan@LAPTOP-6KO4S5LR>
2025-04-28 15:20:32 +0200

    Done with controllers

---
+++ SUT24_JohanHansson_Labb3_API/Controllers/InterestController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SUT24_JohanHansson_Labb3_API.Controllers.Data;
+using SUT24_JohanHansson_Labb3_API.Models;
+
+namespace SUT24_JohanHansson_Labb3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterestController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public InterestController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Interest>>> GetAllInterest()
+        {
+            return await _context.Interests.ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Interest>> CreateInterest(Interest interest)
+        {
+            _context.Interests.Add(interest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAllInterest), new { id = interest.Id}, interest);
+        }
+    }
+}
---
+++ SUT24_JohanHansson_Labb3_API/Controllers/LinksController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SUT24_JohanHansson_Labb3_API.Controllers.Data;
+using SUT24_JohanHansson_Labb3_API.Models;
+
+namespace SUT24_JohanHansson_Labb3_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinksController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public LinksController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("person/{personId}")]
+        public async Task<ActionResult<IEnumerable<Link>>> GetLinks(int personId)
+        {
+            return await _context.Links
+                .Where(l => l.PersonId == personId)
+                .ToListAsync();
+        }
+        [HttpPost]
+        public async Task<ActionResult<Link>> AddLink(Link link)
+        {
+            var personInterest = await _context.PersonInterests
+                .FindAsync(link.PersonId, link.InterestId);
+            if(personInterest == null)
+            {
+                return BadRequest("Personen är har inte detta intresset.");
+            }
+
+            _context.Links.Add(link);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
