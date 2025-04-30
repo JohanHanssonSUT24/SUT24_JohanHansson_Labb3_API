fe446c3313cda6065cec306bac92e9103e659117
johan <johan@LAPTOP-6KO4S5LR>
2025-04-30 19:02:31 +0200

    Done with rebuild

--- SUT24_JohanHansson_Labb3_API/Controllers/Data/APIDbContext.cs
+++ SUT24_JohanHansson_Labb3_API/Controllers/Data/APIDbContext.cs
@@ -7,13 +7,28 @@ namespace SUT24_JohanHansson_Labb3_API.Controllers.Data
     {
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) { }
 
-
         //Create DbSet for Person, Interest, PersonInterest and Link.
         public DbSet<Person> Persons { get; set; }
         public DbSet<Interest> Interests { get; set; }
         public DbSet<PersonInterest> PersonInterests { get; set; }
         public DbSet<Link> Links { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonInterest>()
+                .HasOne(pi => pi.Person)
+                .WithMany(p => p.PersonInterests)
+                .HasForeignKey(pi => pi.PersonId);
 
+            modelBuilder.Entity<PersonInterest>()
+                .HasOne(pi => pi.Interest)
+                .WithMany(i => i.PersonInterests)
+                .HasForeignKey(pi => pi.InterestId);
+
+            modelBuilder.Entity<Link>()
+                .HasOne(l => l.PersonInterest)
+                .WithMany(pi => pi.Links)
+                .HasForeignKey(l => l.PersonInterestId);
+        }
     }
 }
 
